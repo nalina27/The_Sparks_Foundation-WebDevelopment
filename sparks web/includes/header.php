0c07e355e994ddcb4dfe9b94264f0e37bd64f0c0
Nalina N <68369222+nalina27@users.noreply.github.com>
2021-06-20 11:33:12 +0530

    Add files via upload

---
+++ sparks web/includes/header.php
@@ -0,0 +1,10 @@
+
+<a class="navbar-brand"style="color: #cc33ff;">Sparks Bank</a>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="nav navbar-nav navbar-right" >
+<li><a href = "index.php" style="color: #cc33ff;"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
+<li><a href="user.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-user"></span> New Users</a></li>
+<li><a href="transaction.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-transfer"></span> Transfer Money</a></li>
+<li><a href="history.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-inbox"></span> History</a></li>
+</ul>
+</div>
